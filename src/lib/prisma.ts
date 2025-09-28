05c21fb805074e21530bb54e68b4d3754c1cdb98
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-09-27 21:54:23 -0700

    feat: Add prisma instance

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
