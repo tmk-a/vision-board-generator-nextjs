bdc6d024cdbfb53fc58d7d437fdb2ac2a971818a
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-10-01 23:00:49 -0700

    feat: Add chat page

---
+++ src/app/(main)/chat/[conversationId]/page.tsx
@@ -0,0 +1,33 @@
+import { ChatContent } from "../../../../components/features/chat/ChatContents";
+import { getChatHistory } from "../../../../services/chatService";
+import { notFound } from "next/navigation";
+
+interface ChatPageProps {
+  params: {
+    conversationId: string;
+  };
+}
+
+export default async function ChatPage({ params }: ChatPageProps) {
+  const { conversationId } = await params;
+
+  if (conversationId !== "new") {
+    const chatData = await getChatHistory(conversationId);
+
+    if (!chatData) {
+      notFound();
+    }
+
+    const nextQuestionNo = chatData[chatData.length - 1].turn_number + 1;
+
+    return (
+      <ChatContent
+        conversationId={conversationId}
+        initialHistory={chatData}
+        initialQuestionNo={nextQuestionNo}
+      />
+    );
+  }
+
+  return <ChatContent conversationId={conversationId} />;
+}
