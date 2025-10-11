d3f6757d8b385cf8494eac858a787df44f6d95ff
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-10-10 23:51:48 -0700

    feat: Add footer component

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-stone-700 text-white text-center bottom-0 w-full">
+      <p>@2025 All right reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
